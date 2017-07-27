1bf275c0bac195b7475f25267a6af4af2e697fde
Chris Wang <kisa.ayano@icloud.com>
2017-07-26 22:29:14 -0400

    Fix spelling error.
    Signed-off-by: Chris Wang <kisa.ayano@icloud.com>

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,4 +2,4 @@ echo 'Downloading package'
 curl -o Unity.pkg https://unity3d.com/get-unity/download?thank-you=update&download_nid=47505&os=Mac
 
 echo 'Installing'
-sudo isntaller -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity.pkg -target /
