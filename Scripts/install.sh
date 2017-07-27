d1691a21576d89af0a708217a8e75cd2ef51a0d2
Chris Wang <kisa.ayano@icloud.com>
2017-07-26 22:44:21 -0400

    Update download script.
    Signed-off-by: Chris Wang <kisa.ayano@icloud.com>

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,5 +1,29 @@
-echo 'Downloading package'
-curl -o Unity.pkg https://unity3d.com/get-unity/download?thank-you=update&download_nid=47505&os=Mac
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=472613c02cf7
+VERSION=2017.1.0f3
 
-echo 'Installing'
-sudo installer -dumplog -package Unity.pkg -target /
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
+
+http://netstorage.unity3d.com/unity/472613c02cf7/MacEditorInstaller/Unity-2017.1.0f3.pkg?_ga=2.112470284.900733464.1501121406-1384945295.1501121406/
