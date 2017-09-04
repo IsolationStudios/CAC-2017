efb8351497eada7faec23c6a4c114af39f3e8eaf
jasmaa <jasonmaa3955@gmail.com>
2017-09-04 09:58:46 -0400

    Add grayscale shader

---
+++ Assets/Scripts/PostEffectScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Applies post-processing shader onto cam
+ */
+
+public class PostEffectScript : MonoBehaviour {
+	public Material mat;
+
+	public Material grayscaleMat;
+	public Material fadeBackMat;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest){
+		Graphics.Blit (src, dest, mat);
+	}
+
+	public void FadeBack(){
+		mat = fadeBackMat;
+	}
+}
---
+++ Assets/Scripts/test/TEMP_fadebut.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMP_fadebut : MonoBehaviour {
+
+	public void FadeBut(){
+		GameObject.Find ("Player").GetComponent<PostEffectScript> ().FadeBack ();
+	}
+}
