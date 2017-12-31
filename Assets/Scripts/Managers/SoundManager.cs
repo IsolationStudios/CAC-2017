6b2772d9936dcf9875b60356998a5b505b3a3c39
jasmaa <jasonmaa3955@gmail.com>
2017-12-30 22:52:40 -0500

    Add kitchen

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -11,12 +11,13 @@ namespace Managers {
 		public static SoundManager instance;
 
 		/*
-		 * 1st audio src = music
-		 * 2nd audio src = sfx
-		 * 3rd audio src = sfx alt
+		 * 0 = music
+		 * 1 = sfx alt
+		 * 2 = sfx
+		 * 3 = sfx2
 		 */
 		public AudioSource musicPlayer;
-		public AudioSource sfxPlayer;
+		public AudioSource[] soundPlayers;
 		public AudioSource sfxAltPlayer;
 
 		public AudioClip[] musicList;
@@ -28,8 +29,15 @@ namespace Managers {
 			else
 				Destroy (gameObject);
 		}
+		void Start(){
+			soundPlayers = GetComponents<AudioSource> ();
 
-		public void PlaySFX(AudioClip c){
+			musicPlayer = soundPlayers [0];
+			sfxAltPlayer = soundPlayers [1];
+		}
+
+		public void PlaySFX(AudioClip c, int playerNum = 2){
+			AudioSource sfxPlayer = soundPlayers [playerNum];
 			sfxPlayer.clip = c;
 			sfxPlayer.Play ();
 		}
